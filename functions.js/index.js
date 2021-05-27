fbb58a67ddcde4456c96a4aed0f36bee1217f788
Romario Soares <romassoares@hotmail.com>
2021-05-27 18:10:50 -0300

    correção de menu, slide, amount no localstorage

--- functions.js/index.js
+++ functions.js/index.js
@@ -10,42 +10,56 @@ let existAmount = localStorage.getItem('@amount') !== null ? localStorageAmount 
 const purchase = event => {
     document.querySelector('#confir').style.display = 'none'
     const modal = document.querySelector('#confir')
-    modal.style.display = 'flex'
+    modal.style.display = 'block'
     event.innerText = 'COMPRADO!'
 
-    event.setAttribute('style', "background-image: url('../svgs/image_6-removebg-preview 1.svg'); background-position: left top;background-repeat: no-repeat; background-color: var(--a)")
+    event.setAttribute('style', "background-image: url('../svgs/image_6-removebg-preview 1.svg'); background-position: right top;background-repeat: no-repeat; background-color: var(--a)")
     badge()
+    setTimeout(() => { document.addEventListener('click', handleClickOutside, false) }, 200);
 }
 
 const badge = () => {
     let totAmount = JSON.parse(localStorage.getItem('@amount'))
-    const total = totAmount === null ? '0' : totAmount.tot
-    let updatetotal = existAmount.push(total + 1)
-
-    localStorage.setItem('@amount', JSON.stringify({ tot: updatetotal }))
+    const total = totAmount === null ? '0' : totAmount
+    localStorage.setItem('@amount', JSON.stringify(Number(total + 1)))
     updateDom()
 }
 
 const updateDom = () => {
     let totAmount = JSON.parse(localStorage.getItem('@amount'))
-    const total = totAmount === null ? '0' : totAmount.tot
+    const total = totAmount === null ? '0' : totAmount
     document.querySelector('#badge').innerHTML = total
 }
 
 updateDom()
 
+// carousel
 const header = document.querySelector('header')
 var imageCount = 0;
 var currentImage = 0;
 var images = new Array();
 
-if (window.screen.availWidth <= 800) {
-    images[0] = '../img/principal_banner_mobile';
+if (window.outerWidth < 360) {
+    images[0] = '../img/principal_banner_mobile.jpg';
     images[1] = '../img/principal_banner_mobile_02.jpg';
 } else {
     images[0] = '../img/principal_banner_desktop.jpg';
     images[1] = '../img/principal_banner_desktop_02.jpg';
 }
+
+window.onresize = setInterval(() => {
+    if (window.outerWidth > 360) {
+        images[0] = '../img/principal_banner_desktop.jpg';
+        images[1] = '../img/principal_banner_desktop_02.jpg';
+    }
+}, 1000)
+
+window.onresize = setInterval(() => {
+    if (window.outerWidth < 360) {
+        images[0] = '../img/principal_banner_mobile.jpg';
+        images[1] = '../img/principal_banner_mobile_02.jpg';
+    }
+}, 1000)
 
 const preLoadImages = new Array();
 for (var i = 0; i < images.length; i++) {
@@ -59,14 +73,15 @@ for (var i = 0; i < images.length; i++) {
 
 function startSlideShow() {
     if (header && imageCount >= 0) {
-        header.style.backgroundImage = "url(" + images[currentImage] + ")";
-        header.style.backgroundRepeat = "no-repeat";
-        header.style.backgroundSize = "cover";
+        header.style.background = "url(" + images[currentImage] + ")";
+        header.style.backgroundRepeat = "repeat";
+        header.style.backgroundSize = "auto 700px";
 
         currentImage = currentImage + 1;
         if (currentImage > (imageCount - 1)) {
             currentImage = 0;
         }
+
         const title = document.querySelector('#title-img-product')
         const paginate = document.querySelector('#paginate')
         const left = document.querySelector('#left')
@@ -97,6 +112,7 @@ function startSlideShow() {
     }
 }
 startSlideShow();
+// fim carousel
 
 const imgcheck = document.querySelector('#img-hamburger')
 
@@ -116,4 +132,10 @@ imgcheck.onclick = function () {
             document.querySelector('.body').style.display = 'none'
         }
     }
+}
+let modal = document.querySelector(".menu");
+modal.onclick = function () {
+    check.checked = false
+    modal.style.display = "none"
+    document.querySelector('.logo>.triangulo').style.display = 'none'
 }
